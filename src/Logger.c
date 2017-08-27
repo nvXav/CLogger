ad13c2ebd90e846a7bdeeddf2669624ab215c400
nvXav <cheng.x.upsud@gmail.com>
2017-08-27 22:23:45 +0200

    first commit

---
+++ src/Logger.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+
+#include "Logger.c"
+
+
+void save_log()
+{
+
+}
+
+void cancel_save_log()
+{
+
+}
+
+void log_print(char *fmt, ...)
+{
+
+}
+
+void log_warning(char *fmt, ...)
+{
+
+}
+
+void log_error(char *fmt, ...)
+{
+
+}
+
---
+++ src/Logger.h
@@ -0,0 +1,5 @@
+void save_log();
+void cancel_save_log();
+void log_print(char *fmt, ...);
+void log_warning(char *fmt, ...);
+void log_error(char *fmt, ...);
